9a2274d354aca8398707b253908b75e33dd321a4
Maaike Verplancke <112861144+Maaike0904@users.noreply.github.com>
2022-12-12 18:16:44 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let modal= document.querySelector('.modal')
+let openDialog = document.querySelector('.opendialog-button')
+
+openDialog.addEventListener("click", () => { 
+    modal.showModal();
+})
+
